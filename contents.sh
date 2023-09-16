549694e4dbebc7e42bb3103b97a63f81dc54433d
Iamneo <techadmin@iamneo.ai>
2023-09-16 08:50:34 +0000

    Backup Commit

--- contents.sh
+++ contents.sh
@@ -1,10 +1,6 @@
-# Define the file name
+
 file_name="my_text_file.txt"
-
-# Create the file and write some text
 echo "This is some text content." > "$file_name"
-
-# Check if the file was created and written to successfully
 if [ -f "$file_name" ]; then
   echo "File created and text written successfully."
 else
--- dob.sh
+++ dob.sh
@@ -2,5 +2,5 @@ read -p "Enter day (DD): " day
 read -p "Enter month (MM): " month
 read -p "Enter year (YYYY): " year
 
-# Print the date of birth in the desired format
+
 echo "Date of birth: $day-$month-$year"
